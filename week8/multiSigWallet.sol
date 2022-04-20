b652246be66b4480908b834afde61d19e05a0770
Amarachi <49257104+amarachiugwu@users.noreply.github.com>
2022-04-20 11:39:04 +0100

    Update multiSigWallet.sol

--- week8/multiSigWallet.sol
+++ week8/multiSigWallet.sol
@@ -124,3 +124,10 @@ contract MultiSigWallet {
         emit Revoke(_txn_id, msg.sender);
     }
 }
+
+// remember to pass in the _owners array and the required while deploying the contract
+// [
+// "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4",
+// "0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2",
+// "0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db"
+// ],2
